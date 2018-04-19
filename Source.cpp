2275525ec4e4a47216582cb610004df9d816334b
malykh_alex <awa149@rambler.ru>
2018-04-20 00:16:52 +0700

    Source.cpp created

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma comment(linker, "/STACK:10034217728")
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cassert>
+#include <utility>
+#include <ctime>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <random>
+#include <unordered_map>
+#include <list>
+#include <numeric>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+	return 0;
+}
